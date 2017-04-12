68925f03897faa4b8e499915ac2f1b90a17079db
chapeter <chapeter@cisco.com>
2017-04-12 09:24:19 -0500

    Added display for apic ip and user

---
+++ acibootstrap/importvars.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python acibootstrap/importvars.py
--- upload.py
+++ upload.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
+import yaml
 import subprocess
 
 UPLOAD_FOLDER = 'acibootstrap/files/vars/'
@@ -13,10 +14,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-#@app.route("/run", methods=['GET'])
-#def run():
-#    subprocess.call("./acibootstrap.sh")
-#    return redirect("http://0.0.0.0:5000", code=302)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -25,7 +23,19 @@ def index():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            subprocess.call("acibootstrap/importvars.sh")
             return redirect(url_for('index'))
+
+    with open("acibootstrap/files/vars/acibootstrap_vars.yml", 'r') as file:
+        try:
+            vars = yaml.load(file)
+            apic_ip = vars['apic_ip']
+            apic_user = vars['apic_user']
+        except:
+            apic_ip = none
+            apic_user = none
+
+
     return """
     <!doctype html>
     <title>ACI BOOTSTRAP</title>
@@ -37,9 +47,13 @@ def index():
          <input type=submit value=Upload>
     </form>
     <p>%s</p>
+    <p>
+       APIC IP: %s <br>
+       APIC User: %s <br>
+    </p>
     <iframe src="http://0.0.0.0:5001" width="300" height="25"></iframe>
     <iframe src="http://0.0.0.0:8001" width="1200" height="650"></iframe>
-    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+    """ % ("<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],)), apic_ip, apic_user)
 
 
 if __name__ == "__main__":
