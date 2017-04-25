b9c3108760f7307f6d6680b246504ab233cab394
chapeter <chapeter@cisco.com>
2017-04-25 12:26:36 -0500

    ospf, eigrp, and static routing for POC tenant complete

--- run.py
+++ run.py
@@ -9,12 +9,18 @@ def run():
     subprocess.call("./acibootstrap.sh")
     return redirect("http://0.0.0.0:5001", code=302)
 
+@app.route("/tests")
+def tests():
+    subprocess.call("./acibootstrap-tests.sh")
+    return redirect("http://0.0.0.0:5001", code=302)
+
 @app.route("/")
 def main():
     return '''
     <!doctype html>
     <title>ACI BOOTSTRAP</title>
     <a href="http://0.0.0.0:5001/run">Run ACI Bootstrap</a>
+    <a href="http://0.0.0.0:5001/tests">Run tests.yaml</a>
     '''
 
 
