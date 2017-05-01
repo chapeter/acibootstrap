86a5142e74166e843052c445bb04c1509aee4337
chapeter <chapeter@cisco.com>
2017-05-01 14:40:20 -0700

    revert

--- acibootstrap/views.py
+++ acibootstrap/views.py
@@ -45,66 +45,15 @@ def writeHosts():
 
     return
 
-@app.route("/run", methods=['GET'])
-def run():
-    subprocess.call("./acibootstrap.sh")
 
 @app.route("/portmap")
 def portmap():
     return render_template('portmap.html')
 
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join('acibootstrap/files/vars/', filename))
-            #subprocess.call("acibootstrap/importvars.py")
-            importvars()
-            writeHosts()
-            return redirect(url_for('index'))
-
-    with open("acibootstrap/files/vars/acibootstrap_vars.yml", 'r') as file:
-        try:
-            vars = yaml.load(file)
-            apic_ip = vars['apic_ip']
-            apic_user = vars['apic_user']
-        except:
-            apic_ip = None
-            apic_user = None
-
-    # used to show files in dir
-    # <iframe src="http://0.0.0.0:8001" width="1200" height="650"></iframe>
-
-    return """
-    <!doctype html>
-
-    <link rel="stylesheet" href="static/css/bootstrap-theme.min.css" />
-    <link rel="stylesheet" href="static/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="static/css/my.css" />
-    <script src="static/js/bootstrap.min.js"></script>
-
-    <title>ACI BOOTSTRAP</title>
-    <h1>ACI BOOTSTRAP</h1>
-    <h2>Upload Config File</h2>
-    <p>Upload acibootstrap.xlsx here</p>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-      </p>
-    </form>
-    <p>
-       APIC IP: %s <br>
-       APIC User: %s <br>
-    </p>
-    <button type="button" onclick="window.location.href='/run'">Run</button>
-
-    """ % (apic_ip, apic_user)
-
-
-@app.route("/dashboard", methods=['GET', 'POST'])
-def dashboard():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
